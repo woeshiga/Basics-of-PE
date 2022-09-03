ab992a0eb0aa6a5aa6f90489d2aa792abf2a7ae9
woeshiga <egor.grebenckin16@gmail.com>
2022-09-03 12:13:41 +0300

    Added game loop

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ print(f"Your name is {name}\nYour age is {age}\nYour hobby is {hobby}")
 
 print("Nice to meet you! Let's play to my game!")
 
-number = randint(0, 1)
-
 while True:
-    user_number = int(input("Zero or One? (0/1)"))
-    if user_number not in [0, 1]:
+    number = randint(0, 1)
+
+    while True:
+        user_number = int(input("Zero or One? (0/1)"))
+        if user_number not in [0, 1]:
+            break
+
+    if user_number == number:
+        print("Yes!))")
+    else:
+        print("Nooo((")
+
+    answer = input("One more round?;) (y - yes/n- no)")
+
+    if answer == "n":
         break
-
-if user_number == number:
-    print("Yes!))")
-else:
-    print("Nooo((")
-
+    
 input()
