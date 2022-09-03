7c00a9b450a4120400385acce8a1e010089437a5
woeshiga <egor.grebenckin16@gmail.com>
2022-09-03 12:10:37 +0300

    Added flip coin like game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 name = input("Enter your name: ")
 
 age = input("Enter your age: ")
@@ -5,5 +7,19 @@ age = input("Enter your age: ")
 hobby = input("Enter your hobby: ")
 
 print(f"Your name is {name}\nYour age is {age}\nYour hobby is {hobby}")
+
+print("Nice to meet you! Let's play to my game!")
+
+number = randint(0, 1)
+
+while True:
+    user_number = int(input("Zero or One? (0/1)"))
+    if user_number not in [0, 1]:
+        break
+
+if user_number == number:
+    print("Yes!))")
+else:
+    print("Nooo((")
 
 input()
